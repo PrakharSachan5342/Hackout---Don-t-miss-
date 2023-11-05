d5f4ee885115f75d4fa3c103eda0ab1a9a2c0e0b
Prakhar Sachan <91385023+PrakharSachan5342@users.noreply.github.com>
2023-11-05 10:33:13 +0530

    Update and rename page10.js to srcfrontend/page10.js

---
+++ srcfrontend/page10.js
@@ -0,0 +1,21 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <img style={{width: 371, height: 445, left: 0, top: 355, position: 'absolute'}} src="https://via.placeholder.com/371x445" />
+    <img style={{width: 371, height: 276, left: -3, top: 0, position: 'absolute', borderRadius: 10}} src="https://via.placeholder.com/371x276" />
+    <div style={{width: 18, height: 11, left: 326, top: 9, position: 'absolute', background: 'black'}}></div>
+    <div style={{width: 14, height: 11, left: 307, top: 9, position: 'absolute', background: '#151313'}}></div>
+    <div style={{width: 14, height: 10, left: 286, top: 9, position: 'absolute', background: '#191818'}}></div>
+    <div style={{width: 283, height: 66, left: 41, top: 684, position: 'absolute', background: '#CA6A34', borderRadius: 13.85}} />
+    <div style={{width: 364.53, height: 184, left: 6, top: 292, position: 'absolute', color: 'black', fontSize: 18.98, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Garden City's Mdern , Bengaluru</div>
+    <div style={{left: 6, top: 319, position: 'absolute', color: 'black', fontSize: 21.03, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Price - ₹8,000</div>
+    <div style={{width: 141.93, left: 142, top: 525, position: 'absolute', color: '#848689', fontSize: 8.04, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>23, HAL Old Airport Rd, HAL 2nd Stage, Kodihalli, Bengaluru, Karnataka 560008•080 2521 1234</div>
+    <div style={{left: 15, top: 8, position: 'absolute', color: 'black', fontSize: 10.93, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>9:41</div>
+    <div style={{width: 147, height: 58, left: 177, top: 692, position: 'absolute', background: 'rgba(94.76, 60.53, 0, 0.52)', boxShadow: '80px 80px 80px ', borderRadius: 10, filter: 'blur(80px)'}} />
+    <div style={{width: 76, height: 42, left: 259, top: 692, position: 'absolute', background: 'rgba(253, 197, 98, 0.88)', boxShadow: '100px 100px 100px ', borderRadius: 10, filter: 'blur(100px)'}} />
+    <div style={{width: 13.33, height: 12, left: 158, top: 325, position: 'absolute', background: '#D7A906'}}></div>
+    <div style={{width: 13.33, height: 12, left: 173, top: 325, position: 'absolute', background: '#D7A906'}}></div>
+    <div style={{width: 13.33, height: 12, left: 188, top: 325, position: 'absolute', background: '#D7A906'}}></div>
+    <div style={{width: 13.33, height: 12, left: 205, top: 325, position: 'absolute', background: '#D7A906'}}></div>
+    <div style={{left: 112, top: 707, position: 'absolute', color: 'black', fontSize: 17.38, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}> Confirm Booking</div>
+    <div style={{width: 360, height: 0, left: 0, top: 355, position: 'absolute', border: '1px #CA6A34 solid'}}></div>
+    <div style={{width: 363, height: 0, left: -3, top: 276, position: 'absolute', border: '1px #CA6A34 solid'}}></div>
+</div>
