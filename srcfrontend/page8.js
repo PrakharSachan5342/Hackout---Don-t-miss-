2032b6375586c40a8e5ad464fd1da8de15ce632f
Prakhar Sachan <91385023+PrakharSachan5342@users.noreply.github.com>
2023-11-05 10:36:25 +0530

    Update and rename page8.js to srcfrontend/page8.js

---
+++ srcfrontend/page8.js
@@ -0,0 +1,86 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 18, height: 11, left: 326, top: 9, position: 'absolute', background: 'black'}}></div>
+    <div style={{width: 14, height: 11, left: 307, top: 9, position: 'absolute', background: '#151313'}}></div>
+    <div style={{width: 14, height: 10, left: 286, top: 9, position: 'absolute', background: '#191818'}}></div>
+    <div style={{width: 312, height: 130, left: 31, top: 84, position: 'absolute'}}>
+        <div style={{width: 76.65, height: 130, left: 117.82, top: 0, position: 'absolute', background: '#E6E6E6'}}></div>
+        <div style={{width: 92.91, height: 0.75, left: 194.11, top: 32.04, position: 'absolute', background: '#E6E6E6'}}></div>
+        <div style={{width: 6.18, height: 26.29, left: 118.11, top: 86.06, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 3.41, height: 10.25, left: 115.32, top: 105.89, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 3.41, height: 10.25, left: 123.96, top: 105.89, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 36.13, height: 4.01, left: 104.53, top: 67.34, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 140.89, height: 16.04, left: 0, top: 73.25, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 104.87, height: 18.57, left: 33.75, top: 78.40, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 21.59, height: 22.50, left: 93.05, top: 76.48, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 15.91, height: 15.60, left: 95.90, top: 77.93, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 6.18, height: 26.29, left: 187.71, top: 86.06, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 3.41, height: 10.25, left: 193.27, top: 105.89, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 3.41, height: 10.25, left: 184.63, top: 105.89, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 36.13, height: 4.01, left: 171.34, top: 67.34, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 4.09, height: 35.42, left: 155.86, top: 32.24, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 140.89, height: 16.04, left: 171.11, top: 73.25, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 104.87, height: 18.57, left: 173.38, top: 78.40, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 21.59, height: 22.50, left: 197.36, top: 76.48, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 15.91, height: 15.60, left: 200.20, top: 77.93, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 37.40, height: 60.25, left: 138.43, top: 49.80, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 29.43, height: 6.68, left: 143.39, top: 55.16, position: 'absolute', background: '#E6E6E6'}}></div>
+        <div style={{width: 7.73, height: 19.29, left: 153.26, top: 100.28, position: 'absolute'}}>
+            <div style={{width: 3.97, height: 16.86, left: 1.79, top: 0, position: 'absolute', background: '#3F3D56'}}></div>
+            <div style={{width: 2.19, height: 6.57, left: -0, top: 12.72, position: 'absolute', background: '#3F3D56'}}></div>
+            <div style={{width: 2.19, height: 6.57, left: 5.54, top: 12.72, position: 'absolute', background: '#3F3D56'}}></div>
+        </div>
+        <div style={{width: 11.52, height: 0.63, left: 148.52, top: 62.96, position: 'absolute', transform: 'rotate(-74.18deg)', transformOrigin: '0 0', background: '#D82424'}}></div>
+        <div style={{width: 0.63, height: 11.51, left: 165.28, top: 53.76, position: 'absolute', transform: 'rotate(-10.90deg)', transformOrigin: '0 0', background: '#D82424'}}></div>
+        <div style={{width: 8.16, height: 8, left: 226.50, top: 27.94, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 8.16, height: 8, left: 113.53, top: 41.03, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 92.91, height: 0.75, left: 25.19, top: 119.77, position: 'absolute', background: '#E6E6E6'}}></div>
+        <div style={{width: 8.16, height: 8, left: 57.59, top: 115.67, position: 'absolute', background: '#D82424'}}></div>
+    </div>
+    <div style={{width: 360, height: 0, left: 0, top: 75, position: 'absolute', border: '1px #0C0B0B solid'}}></div>
+    <div style={{left: 119, top: 46, position: 'absolute', color: '#CC1212', fontSize: 21.71, fontFamily: 'Inknut Antiqua', fontWeight: '700', lineHeight: 90.46, wordWrap: 'break-word'}}>Safarnama<br/></div>
+    <div style={{left: 93, top: 84, position: 'absolute', color: 'black', fontSize: 14.08, fontFamily: 'Inknut Antiqua', fontWeight: '700', lineHeight: 58.68, wordWrap: 'break-word'}}> Navigating Your World</div>
+    <div style={{width: 93, height: 30, left: 15, top: 27, position: 'absolute', background: '#161414'}} />
+    <div style={{width: 57.13, height: 41, left: 36, top: 17, position: 'absolute', color: 'white', fontSize: 11.78, fontFamily: 'Inknut Antiqua', fontWeight: '700', lineHeight: 49.09, wordWrap: 'break-word'}}>Explore</div>
+    <div style={{width: 360, height: 235, left: 0, top: 565, position: 'absolute'}}>
+        <div style={{width: 38.51, height: 35.83, left: 299.88, top: 8.15, position: 'absolute', background: '#FF6584'}}></div>
+        <div style={{width: 31.22, height: 94.89, left: 231.87, top: 4.30, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 31.22, height: 94.89, left: 161.58, top: 20.04, position: 'absolute', background: '#CBCBCB'}}></div>
+        <div style={{width: 31.22, height: 94.89, left: 292.37, top: 0, position: 'absolute', background: '#3F3D56'}}></div>
+        <div style={{width: 360, height: 143.27, left: 0, top: 91.73, position: 'absolute', background: '#F1F1F1'}}></div>
+        <div style={{width: 277.54, height: 110.45, left: 82.46, top: 124.55, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 247.63, height: 88.36, left: 111.99, top: 141.21, position: 'absolute', background: 'white'}}></div>
+        <div style={{width: 194.19, height: 77.28, left: 165.81, top: 157.72, position: 'absolute', background: '#F1F1F1'}}></div>
+        <div style={{width: 360, height: 71.34, left: 0, top: 163.66, position: 'absolute', opacity: 0.20, background: 'black'}}></div>
+        <div style={{width: 3.59, height: 8.72, left: 87.63, top: 140.14, position: 'absolute', background: '#FFB7B7'}}></div>
+        <div style={{width: 7.84, height: 2.93, left: 84, top: 148.12, position: 'absolute', background: '#2F2E41'}}></div>
+        <div style={{width: 5.90, height: 9.01, left: 106.79, top: 138.77, position: 'absolute', background: '#FFB7B7'}}></div>
+        <div style={{width: 7.70, height: 4.72, left: 106.20, top: 146.33, position: 'absolute', background: '#2F2E41'}}></div>
+        <div style={{width: 32.93, height: 40, left: 80.82, top: 105.80, position: 'absolute', background: '#2F2E41'}}></div>
+        <div style={{width: 10.44, height: 9.71, left: 82.34, top: 65.05, position: 'absolute', background: '#FFB8B8'}}></div>
+        <div style={{width: 24.70, height: 33.36, left: 77.84, top: 75.49, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 13.36, height: 8.51, left: 80.63, top: 62.73, position: 'absolute', background: '#2F2E41'}}></div>
+        <div style={{width: 7.32, height: 9.99, left: 102.62, top: 97.55, position: 'absolute', background: '#FFB8B8'}}></div>
+        <div style={{width: 14.61, height: 26.48, left: 94.54, top: 76.68, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 5.34, height: 10.83, left: 74.47, top: 101.25, position: 'absolute', background: '#FFB8B8'}}></div>
+        <div style={{width: 9.20, height: 27.36, left: 74.35, top: 79.80, position: 'absolute', background: '#D82424'}}></div>
+        <div style={{width: 275, height: 47.58, left: 49, top: 163, position: 'absolute', background: '#D12A2A', borderRadius: 95.16}} />
+        <div style={{left: 150, top: 178, position: 'absolute', color: 'white', fontSize: 14.82, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>PROCEED</div>
+    </div>
+    <div style={{left: 15, top: 217, position: 'absolute', color: 'black', fontSize: 18.36, fontFamily: 'Josefin Sans', fontWeight: '700', lineHeight: 76.49, wordWrap: 'break-word'}}>Trip planning</div>
+    <div style={{left: 15, top: 251, position: 'absolute', color: '#848689', fontSize: 13.65, fontFamily: 'Josefin Sans', fontWeight: '700', lineHeight: 56.87, wordWrap: 'break-word'}}>Days & Places</div>
+    <div style={{width: 247, height: 73, left: 46, top: 307, position: 'absolute', background: '#D9D9D9', borderRadius: 10}} />
+    <div style={{width: 247, height: 73, left: 46, top: 402, position: 'absolute', background: '#D9D9D9', borderRadius: 10}} />
+    <div style={{width: 247, height: 73, left: 49, top: 510, position: 'absolute', background: '#D9D9D9', borderRadius: 10}} />
+    <img style={{width: 94, height: 73, left: 46, top: 308, position: 'absolute', borderRadius: 10}} src="https://via.placeholder.com/94x73" />
+    <div style={{left: 160, top: 309, position: 'absolute', color: '#0A0A0B', fontSize: 12.02, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Day-1</div>
+    <div style={{left: 160, top: 407, position: 'absolute', color: '#0A0A0B', fontSize: 12.02, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Day-2</div>
+    <div style={{left: 160, top: 517, position: 'absolute', color: '#0A0A0B', fontSize: 12.02, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Day-3</div>
+    <div style={{left: 160, top: 327, position: 'absolute', color: '#0A0A0B', fontSize: 11.39, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Vidhana Soudha</div>
+    <img style={{width: 95, height: 72, left: 49, top: 510, position: 'absolute', borderRadius: 10}} src="https://via.placeholder.com/95x72" />
+    <div style={{left: 160, top: 425, position: 'absolute', color: '#0A0A0B', fontSize: 11.39, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Bangalore Palace</div>
+    <div style={{left: 160, top: 535, position: 'absolute', color: '#0A0A0B', fontSize: 11.39, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Ramanagara</div>
+    <div style={{width: 273.34, height: 26.76, left: 160, top: 344, position: 'absolute', color: '#848689', fontSize: 6.49, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>XHHR+VF9, Ambedkar Bheedhi, <br/>Sampangi Rama Nagara, <br/>Bengaluru, Karnataka 560001</div>
+    <div style={{width: 273.34, height: 26.76, left: 159, top: 445, position: 'absolute', color: '#848689', fontSize: 6.49, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>XHHR+VF9, Ambedkar Bheedhi, <br/>Sampangi Rama Nagara, <br/>Bengaluru, Karnataka 560001</div>
+    <div style={{width: 283.55, height: 27.76, left: 160, top: 552, position: 'absolute', color: '#848689', fontSize: 6.73, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>XHHR+VF9, Ambedkar Bheedhi, <br/>Sampangi Rama Nagara, <br/>Bengaluru, Karnataka 560001</div>
+    <img style={{width: 93, height: 74, left: 47, top: 402, position: 'absolute', borderRadius: 10}} src="https://via.placeholder.com/93x74" />
+</div>
